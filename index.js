f5abd41e325680bb765c83d4c39932e31aea76c6
hyunseok <prking94@naver.com>
2023-11-21 18:19:11 +0900

    2023-11-21 index.js User 모듈 Method POST  전송 테스트

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://aodhzld45:abcd@1013@boilerplate.jjqmznv.mongodb.net/?retryWrites=true&w=majority";
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
+
+//User Model Import
+const { User } = require('./models/User');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://aodhzld45:abcd1013@boiler-plate.mju6n4y.mongodb.net/', {
@@ -15,6 +20,35 @@ mongoose.connect('mongodb+srv://aodhzld45:abcd1013@boiler-plate.mju6n4y.mongodb.
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.post('/register', async (req, res) => {
+  // 회원가입시 필요한 정보들을 Client에서 가져오면 DB에 삽입
+
+  // 유저 정보 인스턴스 생성
+  // req.body에는 json 형태로 {id : 'hello'} 데이터가 들어있음
+  const user = new User(req.body);
+
+  try {
+    // await를 사용하여 유저를 저장
+    const savedUser = await user.save();
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      error: '회원가입에 실패했습니다.',
+      details: err.message,
+    });
+  }
+});
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
