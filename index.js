868a6c7f25c7adaff41943066fe00c78daf03a20
hyunseok <prking94@naver.com>
2023-11-21 17:21:29 +0900

    2023-11-21 boilerplate Project MongDB 연결 테스트

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://aodhzld45:abcd@1013@boilerplate.jjqmznv.mongodb.net/?retryWrites=true&w=majority";
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://aodhzld45:abcd1013@boiler-plate.mju6n4y.mongodb.net/', {
+}).then(() => {
+  console.log('MongoDB Connected.. Success');
+}).catch((err) => {
+  console.log(err);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
