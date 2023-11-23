a69205c0bb32188176afc7ef24c13955feb79d74
hyunseok <prking94@naver.com>
2023-11-23 17:54:24 +0900

    2023-11-23 User Module 비밀번호 암호화 추가

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,22 @@ const userSchema = mongoose.Schema({
         type : Number
     }
   
+})
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+// 비밀번호 암호화 시키기
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if (err) return next(err); // 에러처리
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err); // 에러처리
+                user.password = hash; // user.password에 hash값을 할당
+                next();
+            });
+        });
+    } 
 })
 
 const User = mongoose.model('User', userSchema);
