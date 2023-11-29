0cba3a6205bf510b3036d884cb75120c45d12331
hyunseok <prking94@naver.com>
2023-11-29 15:27:19 +0900

    2023-11-29 Client App(router 관련) 기본 Page 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LandingPage from "./components/view/LandingPage/LandingPage"
+import LoginPage from "./components/view/LoginPage/LoginPage"
+import RegisterPage from "./components/view/RegisterPage/RegisterPage"
+
 
 function App() {
   return (
     <div className="App">
-      <h1>APP APP</h1>
-  
-    </div>
+    <Router>
+      <Routes>
+        {/* exact = 정의한 경로가 주어진 URL과 정확하게 일치해야 해당 라우트가 활성화 */}
+        <Route exact path="/" element = { <LandingPage /> } /> 
+        <Route path="/login" element = { <LoginPage /> } />
+        <Route path="/register" element = { <RegisterPage /> } />
+
+      </Routes>
+    </Router>
+  </div>
+
+
   );
 }
 
