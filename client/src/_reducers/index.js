2104d171ab26463103b897c5e84ce0b370612fd9
hyunseok <prking94@naver.com>
2023-12-01 16:12:55 +0900

    2023-12-01 Redux 초기 설정 작업

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux"; // 여러가지 Reducers를 결합하는 combineReducers
+// import user from './user_reducers';
+
+const rootReducer = combineReducers({
+    // user
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkers from './serviceWorker'
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
+import promiseMiddleware from 'redux-promise';
+import { applyMiddleware, createStore } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers'
 
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <Provider
+    store = {createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
